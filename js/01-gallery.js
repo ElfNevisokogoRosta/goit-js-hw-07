c9e5a8d9a9d4ce3ac98d43ca49525ba513dc7f81
ElfNevisokogoRosta <95233843+ElfNevisokogoRosta@users.noreply.github.com>
2022-06-08 05:26:28 +0300

    v1.3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const imagesEl = galleryItems.map(({preview, description, original}) => 
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`).join("");
+galleryEl.insertAdjacentHTML('beforeend', imagesEl);
+galleryEl.addEventListener('click', modalIn);
+function  modalIn(event){
+  event.preventDefault();
+  const isImage = event.target.classList.contains('gallery__image');
+  if(!isImage){
+    return;
+  };
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            <img class="gallery__image" src="${event.target.dataset.source}">
+        </p>
+    </div>
+`);
+  instance.show()
+  const modalEl = document.querySelector('.basicLightbox');
+  modalEl.addEventListener('click', ()=> modalEl.remove());
+};
